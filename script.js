bd9ca2f9f569870d2c9a44079595d5325695b9cb
amontheape <mcanedotome@gmail.com>
2021-11-16 01:43:18 -0300

    feat: send message

--- script.js
+++ script.js
@@ -7,14 +7,30 @@ function loadPage() {
             button.click();
         };
     });
+
+    document.querySelector("footer input").addEventListener("keyup", (pressed)=>{
+        if(pressed.keyCode === 13){
+            sendMessage();
+        };
+    });
 }
 
+let userName;
+
 function getName() {
-    const name = document.querySelector(".wrapper input").value;
+    userName = document.querySelector(".wrapper input").value;
 
-    const nameCheckPromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", {name});
+    const nameCheckPromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/participants", {name: userName});
 
-    nameCheckPromise.then(loadChat);
+    nameCheckPromise.then(()=> {
+        loadChat();
+
+        setInterval(()=> {
+            const onlineStatusPromise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", {name: userName});
+            onlineStatusPromise.then(console.log("post status OK"));
+        }, 5000);
+    });
+
     nameCheckPromise.catch(nameError);
 }
 
@@ -28,38 +44,52 @@ function nameError(error) {
 }
 
 function loadChat() {
+
     const getServerMessagesPromise = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
 
     getServerMessagesPromise.then( ( {data} )=>{
 
         const messageBox = document.querySelector(".messages-box");
 
-        data.forEach( message => {
-            messageBox.innerHTML += messageCustomizer(message);     
+        messageBox.innerHTML = "";
+
+        const filteredData = data.filter((message)=> !(message.type === 'private_message' && message.to !== userName));
+
+        console.log(filteredData);
+
+        filteredData.forEach( message => {
+            messageBox.innerHTML += messageCustomizer(message);
+
+            const newScroll = document.querySelectorAll(".msg");
+            newScroll[newScroll.length - 1].scrollIntoView(); 
         });
 
     });
+
+    setInterval(loadChat, 20000);
 }
 
-function messageCustomizer( {from, to, text, type, time} ) {
+function messageCustomizer( {from, to, text, type, time='agora'} ) {
+    
     switch (type) {
         case 'status': 
             return `<div class="msg status">
-                        <span class="time">${time}</span>
+                        <span class="time">(${time})</span>
                         <strong>${from}</strong>
                         ${text}
                     </div>`;
 
         case 'message':
             return `<div class="msg normal"> 
-                        <span class="time">${time}</span>
+                        <span class="time">(${time})</span>
                         <strong>${from}</strong> para<strong>${to}</strong>:
                         ${text}
                     </div>`;
 
         case 'private_message':
+            if (to !== userName) break;
             return `<div class="msg private">
-                        <span class="time">${time}</span>
+                        <span class="time">(${time})</span>
                         <strong>${from}</strong> reservadamente para<strong>${to}</strong>:
                         ${text}
                     </div>`;
@@ -67,6 +97,31 @@ function messageCustomizer( {from, to, text, type, time} ) {
         default :
             break;
     }
+}
+
+function sendMessage() {
+
+    const footerInput = document.querySelector("footer input");
+
+    const currentMsg = {
+        from: userName,
+        to: "Todos",
+        text: footerInput.value,
+        type: "message"
+    };
+    
+    const messageSent = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", currentMsg);
+
+    const messageBox = document.querySelector(".messages-box");
+
+    messageBox.innerHTML += messageCustomizer(currentMsg);
+
+    const newScroll = document.querySelectorAll(".msg");
+    newScroll[newScroll.length - 1].scrollIntoView();
+
+    footerInput.value = '';
+
+    messageSent.then((response)=>{console.log(response)}, (error)=>{console.log(error)});
 }
 
 loadPage();
